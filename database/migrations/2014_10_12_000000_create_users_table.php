8a6502cac06aae176255f6dc90b106ad53d89add
nandocahyo <nandocahyo77@gmail.com>
2020-08-03 21:29:12 +0700

    create migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger("job_id");
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string("image");
+            $table->boolean("gender");
+            $table->string("phone_number");
             $table->rememberToken();
             $table->timestamps();
         });

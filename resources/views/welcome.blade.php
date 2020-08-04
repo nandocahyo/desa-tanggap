83fae1abefb14273c5f95c615524dabaf2ed7cba
nandocahyo <nandocahyo77@gmail.com>
2020-08-04 17:15:05 +0700

    finishing dan landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,171 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="en">
 
-        <title>Laravel</title>
+<head>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+    <title>Pendataan Warga Desa</title>
 
-            .full-height {
-                height: 100vh;
-            }
+    <!-- Bootstrap core CSS -->
+    <link href="{{ asset('assets/landing_page/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('assets/admin_template/modules/fontawesome/css/all.min.css') }}">
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+    <!-- Custom fonts for this template -->
+    <link href="{{ asset('assets/landing_page/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('assets/landing_page/vendor/simple-line-icons/css/simple-line-icons.css') }}">
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Muli" rel="stylesheet">
 
-            .position-ref {
-                position: relative;
-            }
+    <!-- Plugin CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/landing_page/device-mockups/device-mockups.min.css') }}">
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+    <!-- Custom styles for this template -->
+    <link href="{{ asset('assets/landing_page/css/new-age.min.css') }}" rel="stylesheet">
 
-            .content {
-                text-align: center;
-            }
+</head>
 
-            .title {
-                font-size: 84px;
-            }
+<body id="page-top">
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+        <div class="container">
+            <a class="navbar-brand js-scroll-trigger" href="#page-top">Pendataan Warga Desa</a>
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+                data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
+                aria-label="Toggle navigation">
+                Menu
+                <i class="fas fa-bars"></i>
+            </button>
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="{{ route('login') }}">Login</a>
+                    </li>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="{{ route('register') }}">Register</a>
+                    </li>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="#about">About</a>
+                    </li>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="#features">Fitur</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <header class="masthead">
+        <div class="container h-100">
+            <div class="row h-100">
+                <div class="col-lg-7 my-auto">
+                    <div class="header-content mx-auto">
+                        <h1 class="mb-5">Selamat datang pada aplikasi pendataan warga desa! Daftar sekarang untuk mulai
+                            menggunakan.</h1>
+
+                        <a href="{{ route('register') }}" class="btn btn-outline btn-xl js-scroll-trigger">Register</a>
+                    </div>
                 </div>
             </div>
         </div>
-    </body>
+    </header>
+
+    <section class="download bg-primary text-center" id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 mx-auto">
+                    <h2 class="section-heading">Apa kegunaan aplikasi ini?</h2>
+
+                    <p>Aplikasi ini berguna untuk melakukan pendataan terhadap aktivitas keluar masuknya warga yang ada
+                        pada desa ini. Aplikasi ini diciptakan dengan harapan menciptakan efektifitas dan menghindari
+                        kontak fisik antar manusia.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="features" id="features">
+        <div class="container">
+            <div class="section-heading text-center">
+                <h2>Fitur</h2>
+                <p class="text-muted">Berikut adalah fitur yang ada pada aplikasi ini.</p>
+                <hr>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-8 my-auto mx-auto">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <div class="feature-item">
+                                    <i class="fa fa-envelope text-primary"></i>
+                                    <h3>Laporan Mandiri</h3>
+                                    <p class="text-muted">Pengguna dapat melakukan laporan mandiri mengenai
+                                        aktivitasnya.
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-6">
+                                <div class="feature-item">
+                                    <i class="fa fa-history text-primary"></i>
+                                    <h3>Riwayat Laporan</h3>
+                                    <p class="text-muted">Pengguna dapat melihat semua riwayat laporan yang pernah
+                                        dilakukan.</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <div class="feature-item">
+                                    <i class="fa fa-globe text-primary"></i>
+                                    <h3>Jangkauan Luas</h3>
+                                    <p class="text-muted">Aplikasi ini mendukung seluruh provinsi dan kabupaten/kota
+                                        yang ada di Indonesia.</p>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-6">
+                                <div class="feature-item">
+                                    <i class="icon-present text-primary"></i>
+                                    <h3>Gratis</h3>
+                                    <p class="text-muted">Tidak ada pungutan biaya untuk pemakaian aplikasi ini.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer>
+        <div class="container">
+            <p>&copy; 2020. All Rights Reserved.</p>
+        </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="{{ asset('assets/landing_page/vendor/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/landing_page/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="{{ asset('assets/landing_page/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="{{ asset('assets/landing_page/js/new-age.min.js') }}"></script>
+
+</body>
+
 </html>
